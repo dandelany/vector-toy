8904959e5fd1e8cb754843b2be38c05ab99f6a42
Dan Delany <dan.delany@gmail.com>
2016-02-04 01:28:35 -0500

    color change

--- build/bundle.js
+++ build/bundle.js
@@ -20081,6 +20081,8 @@
 	
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 	
+	window.d3 = _d2.default;
+	
 	var VectorWallpaper = (0, _Wallpaper2.default)((_temp = _class = function (_React$Component) {
 	    _inherits(VectorContainer, _React$Component);
 	
